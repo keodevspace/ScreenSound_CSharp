25b7390a292eddf0dda618d6aa1b4509b5759c3c
KEO <keoxcoelho@gmail.com~>
2023-10-09 17:03:04 -0300

    uploading files

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,30 @@
+﻿// Screen Sound
+
+string boasVindas = @"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░";
+
+void ExibirMensagemDeBoasVindas()
+{
+    Console.WriteLine(boasVindas);
+    Console.WriteLine("\nBoas Vindas ao Screen Sound!");
+}
+
+void ExibirOpcoesMenu()
+{
+    Console.WriteLine("\nMenu Principal");
+    Console.WriteLine("1 - Cadastrar Banda");
+    Console.WriteLine("2 - Mostrar Bandas");
+    Console.WriteLine("3 - Avaliar Banda");
+    Console.WriteLine("4 - Exibir Média de Banda");
+    Console.WriteLine("E - Sair");
+    Console.WriteLine("\nDigite a opção desejada:");
+ }
+
+
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesMenu();
